0227f64f5c750233a082ae1e629c192987c14536
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-20 16:17:06 +0100

    add asserts

---
+++ Assets/CameraMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoving : MonoBehaviour
+{
+    float speed = 10;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide mouse cursor;
+        // Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        print(input);
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        player.Translate(moveAmount);
+    }
+}
---
+++ Assets/Raycast.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public Transform objectToPlace;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = // TODO
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast (ray, out hitInfo)) {
+            objectToPlace.position = hitInfo.point;
+            objectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal)
+        }
+
+    }
+}

86a7cd7afe257ec42ff7fd99f8f11fdadfb1f296
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-01-21 10:06:47 +0100

    update stuff

---
+++ Assets/PlayerCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+
+    public Transform objectToPlace;
+    public Camera MainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            objectToPlace.position = hitInfo.point;
+            objectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        }
+
+    }
+}

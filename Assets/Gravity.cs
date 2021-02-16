5b29810170a498ad951975f5bf14463f98640931
Jan Straub <Jan.Straub@stud.uni-heidelberg.de>
2021-02-16 15:42:25 +0100

    added gravity

---
+++ Assets/Gravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public float rayLength = 5;
+    public float detectRayLength = 3;
+
+    public LayerMask mask;
+
+    public Transform objectToPlace;
+
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, rayLength, mask)) {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            print(hitInfo.collider.gameObject.name);
+            print(hitInfo.distance);
+
+            objectToPlace.position = hitInfo.point;
+            objectToPlace.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+
+        } else {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * detectRayLength, Color.green);
+        }
+    }
+}

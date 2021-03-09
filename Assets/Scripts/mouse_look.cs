0841526011de3817d7ad4c19942a493d7e1fef89
Paavo0 <74006501+Paavo0@users.noreply.github.com>
2021-03-09 16:57:44 +0100

    alter way for raycast

---
+++ Assets/Scripts/mouse_look.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_look : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        RaycastHit hit;
+        Physics.Raycast(playerBody.position, -playerBody.up, out hit, Mathf.Infinity);
+
+        //transform.up = hit.normal;
+    }
+}
---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class player_movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float movement_speed = 10f;
+    public float gravity = 0.1f;
+    public float jumpHeight = 0.05f;
+
+    private float currentG = 0;
+    private Vector3 graviDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotation();
+        controller.Move(Movement() + ApplyGravity());
+    }
+
+
+    Vector3 Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z) * movement_speed * Time.deltaTime;
+        return move;
+    }
+
+
+    Vector3 ApplyGravity()
+    {
+
+        if (Input.GetButtonDown("Jump") && onGround())
+        {
+            currentG = -jumpHeight;
+        }
+
+        if (onGround())
+        {
+            if (currentG > 0f)
+            {
+                currentG = 0f;
+            }
+        }
+
+        currentG += gravity * Time.deltaTime;
+
+        return graviDir * -currentG;
+    }
+
+    void Rotation()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity);
+        Debug.DrawRay(hit.point, hit.normal * 10, Color.red);
+
+        graviDir = Vector3.Normalize(hit.normal);
+
+        transform.up = hit.normal;
+    }
+
+    bool onGround()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity);
+
+        if(hit.distance < 1f)
+        {
+            return true;
+        } 
+        else
+        {
+            return false;
+        }
+    }
+}

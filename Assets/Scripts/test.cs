dd636d8888b7b738ced7eb4a5802aa0c9fd9b056
Jan Straub <Straub.JanM@gmail.com>
2021-03-25 14:55:57 +0100

    tests

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public float value = 5f;
+    public Transform playeTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = playeTransform.transform.position - Vector3.forward * 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Vector3.up * value);
     }
 }

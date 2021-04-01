f75471d7b3b1e36b6f7be782f1026fc18405c316
Jan Straub <Straub.JanM@gmail.com>
2021-04-01 14:37:43 +0200

    portal implemented

--- Assets/Scripts/portal.cs
+++ Assets/Scripts/portal.cs
@@ -2,49 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Portal : MonoBehaviour {
 
-	public Transform player;
-	public Transform reciever;
+    public CharacterController controller;
+    public Transform player;
+    public Transform portalA;
+    public Transform portalB;
+    private bool playerIsOverlapping = false;
 
-	private bool playerIsOverlapping = false;
-
-	// Update is called once per frame
-	void Update () {
-		if (playerIsOverlapping)
-		{
-			Vector3 portalToPlayer = player.position - transform.position;
+    void Update() {
+        if (playerIsOverlapping) {
+            Vector3 portalToPlayer = player.position - transform.position;
 			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
 			// If this is true: The player has moved across the portal
 			if (dotProduct < 0f)
 			{
 				// Teleport him!
-				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
-				rotationDiff += 180;
-				player.Rotate(Vector3.up, rotationDiff);
-
-				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
-				player.position = reciever.position + positionOffset;
+	
+				player.position = portalB.position;
+                print("woosch");
 
 				playerIsOverlapping = false;
+                // controller.enable = true;
 			}
-		}
-	}
+        }
+    }
 
-	void OnTriggerEnter (Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			playerIsOverlapping = true;
-		}
-	}
-
-	void OnTriggerExit (Collider other)
-	{
-		if (other.tag == "Player")
-		{
-			playerIsOverlapping = false;
-		}
-	}
+    void OnTriggerEnter() {
+        playerIsOverlapping = true;
+        // controller.enable = false;
+        print("teleported");
+    }
 }

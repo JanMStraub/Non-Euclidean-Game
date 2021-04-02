40ae92a531a35cecf647973f2fa29469ecc250f0
Jan Straub <Straub.JanM@gmail.com>
2021-04-02 10:15:19 +0200

    portal fix

--- Assets/Scripts/portal.cs
+++ Assets/Scripts/portal.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Portal : MonoBehaviour {
+public class portal : MonoBehaviour {
 
-    public CharacterController controller;
     public Transform player;
-    public Transform portalA;
-    public Transform portalB;
+    public Transform start;
+    public Transform destination;
     private bool playerIsOverlapping = false;
 
     void Update() {
         if (playerIsOverlapping) {
-            Vector3 portalToPlayer = player.position - transform.position;
-			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+            Vector3 portalToPlayer = player.position - start.position;
+			float dotProduct = Vector3.Dot(start.up, portalToPlayer);
 
 			// If this is true: The player has moved across the portal
 			if (dotProduct < 0f)
 			{
 				// Teleport him!
 	
-				player.position = portalB.position;
+				player.position = destination.position;
                 print("woosch");
 
 				playerIsOverlapping = false;

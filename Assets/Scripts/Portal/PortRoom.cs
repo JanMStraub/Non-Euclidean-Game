d92bafa2ed75eaf322415fd7a9e29c52305ba621
Paavo0 <74006501+Paavo0@users.noreply.github.com>
2021-04-11 12:35:32 +0200

    portal added

---
+++ Assets/Scripts/Portal/PortRoom.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortRoom : MonoBehaviour
+{
+    public bool[] trigger = new bool[10];
+
+    Transform exit;
+    Transform entrence;
+
+    Transform playerTransform;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = new bool[10];
+        playerTransform = GameObject.Find("Player").transform;
+
+        print(playerTransform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(Vector3.Angle(-GameObject.Find("Plane0").transform.up, playerTransform.forward));
+        for (int i = 0; i<8; i++)
+        {
+            if(trigger[i] == true)
+            {
+                entrence = GameObject.Find("Plane" + i).transform;
+                if(i%2 == 0)
+                {
+                    offset = playerTransform.position - entrence.position;
+
+                }
+                else
+                {
+                    offset = playerTransform.position - entrence.position - transform.up;
+                }
+                FindExit(i);
+                trigger[i] = false;
+                Teleport();
+            }
+        }
+        for (int j=8; j<10; j++)
+        {
+            if(trigger[j] == true)
+            {
+                entrence = GameObject.Find("Plane" + j).transform;
+                print(Vector3.Angle(playerTransform.up, entrence.position - playerTransform.position));
+
+                if (Vector3.Angle(playerTransform.up, entrence.position-playerTransform.position) < 90)
+                {
+                    offset = Vector3.zero;
+                }
+                else
+                {
+                    offset = (playerTransform.position - entrence.position) * 1.5f;
+                }
+                string exitName = "Plane" + (((j + 1) % 2)+8);
+                print(exitName);
+                exit = GameObject.Find(exitName).transform;
+
+                //offset = (exit.transform.position - entrence.transform.position) * 0.25f;
+
+                trigger[j] = false;
+                Teleport();
+            }
+        }
+    }
+
+
+    void FindExit(int entrenceNumber)
+    {
+        string exitName = "";
+        
+        exitName = "Plane" + ((entrenceNumber + 4) % 8);
+
+        print(entrence);
+        print(exit);
+
+        exit = GameObject.Find(exitName).transform;
+    }
+
+
+    void Teleport()
+    {
+        playerTransform.position = exit.position + offset + playerTransform.forward;
+    }
+
+    void Teleport1()
+    {
+        playerTransform.position = exit.position + offset + exit.up;
+    }
+}
---
+++ Assets/Scripts/Portal/TriggerReport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerReport : MonoBehaviour
+{
+    PortRoom portRoom;
+    public int id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        portRoom = GameObject.Find("PortalRoomEven").GetComponent<PortRoom>();
+
+        print(id);
+        print(portRoom.trigger[id]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        portRoom.trigger[id] = true;
+    }
+
+}
